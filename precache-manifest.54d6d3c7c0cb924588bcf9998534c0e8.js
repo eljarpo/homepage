b70e6b6463cc04ce4e6a58e2bbf07063d0a4b52b
Jorge Romussi <jromussi@gmail.com>
2019-09-09 18:20:28 -0300

    Updates

---
+++ precache-manifest.54d6d3c7c0cb924588bcf9998534c0e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c6cc46f5a43255ce4edbef7326a905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0568eafcc5a62e07033e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2fd70ca0c8419b71d6f4",
+    "url": "/static/js/2.a3abfdb5.chunk.js"
+  },
+  {
+    "revision": "0568eafcc5a62e07033e",
+    "url": "/static/js/main.e9b904a5.chunk.js"
+  },
+  {
+    "revision": "21dc801212bd04bdc242",
+    "url": "/static/js/runtime~main.c7838c4d.js"
+  }
+]);
